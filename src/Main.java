1c311bf8b9a38193c1cfe2c991ecd6675f2088ec
KosKazzz <xuzzzz@gmail.com>
2022-08-26 19:09:32 +0300

    task-two

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args ){
+    public static void main(String[] args) {
         PersonsComparator pComp = new PersonsComparator();
+        PersonsComparator pCompWithLimit = new PersonsComparator(3);
         List<Person> people = new ArrayList<>();
         //TreeSet<Person> people = new TreeSet<>(pComp);
-        people.add(new Person("Diego","Soto Palma", 25 ));
-        people.add(new Person("Cristóbal","de la Cruz Escobar", 44 ));
-        people.add(new Person("Cristián","Álvarez del Rí", 34 ));
-        people.add(new Person("Emilia","ACastro Molina", 31 ));
-        people.add(new Person("María","Dolores Navarrete Marín", 27 ));
-        people.add(new Person("Natalia","Escobar Carrasco de la Cruz", 21 ));
+        people.add(new Person("Diego", "Soto Palma", 25));
+        people.add(new Person("Cristóbal", "de la Cruz Escobar", 44));
+        people.add(new Person("Cristián", "Álvarez del Rí", 34));
+        people.add(new Person("Emilia", "ACastro Molina", 31));
+        people.add(new Person("María", "Dolores Navarrete Marín", 27));
+        people.add(new Person("Natalia", "Escobar Carrasco de la Cruz", 21));
         //before sort
         for (Person p : people) {
             System.out.println(p.toString());
@@ -18,6 +19,12 @@ public class Main {
         System.out.println();
         people.sort(pComp);
         // after sort
+        for (Person p : people) {
+            System.out.println(p.toString());
+        }
+        System.out.println();
+        // after sort 2
+        people.sort(pCompWithLimit);
         for (Person p : people) {
             System.out.println(p.toString());
         }
--- src/PersonsComparator.java
+++ src/PersonsComparator.java
@@ -1,21 +1,52 @@
 import java.util.Comparator;
 
 public class PersonsComparator implements Comparator<Person> {
+    private int maxCountWords;
+
+    public PersonsComparator(int maxCount) {
+        this.maxCountWords = maxCount;
+    }
+
+    public PersonsComparator() {
+    }
+
     @Override
     public int compare(Person o1, Person o2) {
         String[] surArrayOne = o1.getSurname().split(" ");
         String[] surArrayTwo = o2.getSurname().split(" ");
-        if(surArrayOne.length > surArrayTwo.length){
-            return 1;
-        } else if (surArrayOne.length < surArrayTwo.length) {
-            return  -1;
-        } else {
-            if(o1.getAge() > o2.getAge()){
+        if (maxCountWords <= 0 || (maxCountWords > surArrayOne.length && maxCountWords > surArrayTwo.length)) {
+            if (surArrayOne.length > surArrayTwo.length) {
                 return 1;
-            } else if (o1.getAge() < o2.getAge()) {
+            } else if (surArrayOne.length < surArrayTwo.length) {
                 return -1;
+            } else {
+                return ageCompare(o1.getAge(), o2.getAge());
+            }
+        } else if (maxCountWords <= surArrayOne.length && maxCountWords <= surArrayTwo.length) {
+            return ageCompare(o1.getAge(), o2.getAge());
+        } else if (maxCountWords < surArrayOne.length) {
+            if (surArrayTwo.length < maxCountWords) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (maxCountWords < surArrayTwo.length) {
+            if (surArrayOne.length < maxCountWords) {
+                return -1;
+            } else {
+                return 1;
             }
         }
         return 0;
     }
+
+    public int ageCompare(int age1, int age2) {
+        if (age1 > age2) {
+            return 1;
+        } else if (age1 < age2) {
+            return -1;
+        }
+        return 0;
+    }
+
 }
