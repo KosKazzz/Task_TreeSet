b7e00f9846c1f79f0d63d4de739136f7460513b0
KosKazzz <xuzzzz@gmail.com>
2022-09-01 21:40:33 +0300

    feat:add predicate for remove

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
         PersonsComparator pComp = new PersonsComparator();
         PersonsComparator pCompWithLimit = new PersonsComparator(3);
         List<Person> people = new ArrayList<>();
-        //TreeSet<Person> people = new TreeSet<>(pComp);
         people.add(new Person("Diego", "Soto Palma", 25));
         people.add(new Person("Cristóbal", "de la Cruz Escobar", 44));
         people.add(new Person("Cristián", "Álvarez del Rí", 34));
         people.add(new Person("Emilia", "ACastro Molina", 31));
         people.add(new Person("María", "Dolores Navarrete Marín", 27));
         people.add(new Person("Natalia", "Escobar Carrasco de la Cruz", 21));
+        people.add(new Person("Emilia", "Molina de la Cruz", 17));
+        people.add(new Person("Diego", "Palma de la Rí", 5));
         //before sort
         for (Person p : people) {
             System.out.println(p.toString());
@@ -25,6 +26,12 @@ public class Main {
         System.out.println();
         // after sort 2
         people.sort(pCompWithLimit);
+        for (Person p : people) {
+            System.out.println(p.toString());
+        }
+        System.out.println();
+        //after remove
+        people.removeIf(person -> person.getAge() < 18);
         for (Person p : people) {
             System.out.println(p.toString());
         }
